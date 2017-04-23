836fc250c0ca6e482cefd3b4864b547d9179ef51
jacobcheatley <jacobcheatley@gmail.com>
2017-04-24 11:03:32 +1200

    Eye

---
+++ Assets/Scripts/EyeFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EyeFollow : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Vector3 prevMousePosition;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        float verticalDrill = Input.GetAxisRaw("VerticalDrill");
+        float horizontalDrill = Input.GetAxisRaw("HorizontalDrill");
+        float currentAngle = transform.rotation.eulerAngles.z;
+        float newAngle = currentAngle;
+
+        if (verticalDrill != 0 || horizontalDrill != 0)
+            newAngle = Mathf.Atan2(verticalDrill, horizontalDrill) * Mathf.Rad2Deg;
+        else if (Input.mousePosition != prevMousePosition)
+        {
+            prevMousePosition = Input.mousePosition;
+            Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            newAngle = Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x) * Mathf.Rad2Deg;
+        }
+
+        transform.rotation = Quaternion.AngleAxis(Mathf.LerpAngle(currentAngle, newAngle, Time.deltaTime * 20f), Vector3.forward);
+    }
+}
